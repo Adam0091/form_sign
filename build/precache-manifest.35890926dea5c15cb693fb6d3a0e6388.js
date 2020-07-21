14b7264ffff0565b69f37019d954b34723ddd182
Adam <chernobrovkinandrey@mail.ru>
2020-07-21 21:50:11 +0400

    add build

---
+++ build/precache-manifest.35890926dea5c15cb693fb6d3a0e6388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b761aaa3180cc915f200ac3599bed833",
+    "url": "/index.html"
+  },
+  {
+    "revision": "594c6867ed8e487bd0cf",
+    "url": "/static/css/main.8dd2bb12.chunk.css"
+  },
+  {
+    "revision": "21ee00c88bf3a4614d1c",
+    "url": "/static/js/2.cfb341e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cfb341e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "594c6867ed8e487bd0cf",
+    "url": "/static/js/main.c2322fcd.chunk.js"
+  },
+  {
+    "revision": "c5f758ab953c6d6f2987",
+    "url": "/static/js/runtime-main.63661cca.js"
+  }
+]);
