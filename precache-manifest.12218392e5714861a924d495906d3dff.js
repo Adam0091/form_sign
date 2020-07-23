44713eb09c45d8ad3ad97da26f319abbe5f60477
Adam <adam.holo@mail.ru>
2020-07-23 14:13:04 +0400

    Updates

---
+++ precache-manifest.12218392e5714861a924d495906d3dff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc03d3227d60b1dd38283b7b81b54fcb",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "820c8d56e0c7bb3a927f",
+    "url": "/build/static/css/main.82b05af0.chunk.css"
+  },
+  {
+    "revision": "21ee00c88bf3a4614d1c",
+    "url": "/build/static/js/2.cfb341e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/build/static/js/2.cfb341e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820c8d56e0c7bb3a927f",
+    "url": "/build/static/js/main.d2a558e4.chunk.js"
+  },
+  {
+    "revision": "8b1cc24377a089b0a7b5",
+    "url": "/build/static/js/runtime-main.f73b2503.js"
+  }
+]);
